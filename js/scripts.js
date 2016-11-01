d881f833573659ef10faad55f4c169059a7af76e
Vladimir Nimchyn <nima.vova@gmail.com>
2016-11-02 00:39:57 +0200

    changed js and img

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,31 @@
 $(document).ready(function(){
 
 
+/*
 
-
-    for(var i = 2; i <= 15; i++) {
+    for(var i = 1; i <= 14; i++) {
 
 
         (function(i) {
-            if (i==2) var brImage='url(/images/bgimages/swa_intro-animation-2-test.jpg)';
-            if (i==3) var brImage='url(/images/bgimages/swa_intro-animation-3-test.jpg)';
-            if (i==4) var brImage='url(/images/bgimages/swa_intro-animation-4-test.jpg)';
-            if (i==5) var brImage='url(/images/bgimages/swa_intro-animation-5-test.jpg)';
-            if (i==6) var brImage='url(/images/bgimages/swa_intro-animation-6-test.jpg)';
-            if (i==7) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-1-test.jpg)';
-            if (i==8) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-1-test1.jpg)';
-            if (i==9) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-2-test.jpg)';
-            if (i==10) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-3-test.jpg)';
-            if (i==11) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-4-test.jpg)';
-            if (i==12) var brImage='url(/images/bgimages/swa-tail-down-btn-animation-1-test.jpg)';
-            if (i==13) var brImage='url(/images/bgimages/swa-tail-down-btn-animation-2-test.jpg)';
-            if (i==14) var brImage='url(/images/bgimages/swa-tail-animation-3-test.jpg)';
-            if (i==15) var brImage='url(/images/bgimages/swa-tail-animation-3-test-FINISH.jpg)';
+			if (i==1) var brImage='url(images/bgimages/swa_intro-animation-1-test.jpg)';
+            if (i==2) var brImage='url(images/bgimages/swa_intro-animation-2-test.jpg)';
+            if (i==3) var brImage='url(images/bgimages/swa_intro-animation-3-test.jpg)';
+            if (i==4) var brImage='url(images/bgimages/swa_intro-animation-4-test.jpg)';
+            if (i==5) var brImage='url(images/bgimages/swa_intro-animation-5-test.jpg)';
+            if (i==6) var brImage='url(images/bgimages/swa_intro-animation-6-test.jpg)';
+            if (i==7) var brImage='url(images/bgimages/swa-scroll-down-btn-animation-1-test.jpg)';
+            if (i==8) var brImage='url(images/bgimages/swa-scroll-down-btn-animation-2-test.jpg)';
+            if (i==9) var brImage='url(images/bgimages/swa-scroll-down-btn-animation-3-test.jpg)';
+            if (i==10) var brImage='url(images/bgimages/swa-scroll-down-btn-animation-4-test.jpg)';
+            if (i==11) var brImage='url(images/bgimages/swa-tail-animation-1-test.jpg)';
+            if (i==12) var brImage='url(images/bgimages/swa-tail-animation-2-test.jpg)';
+            if (i==13) var brImage='url(images/bgimages/swa-tail-animation-3-test.jpg)';
+            if (i==14) var brImage='url(images/bgimages/background_finish.PNG)';
+            
 
 
        setTimeout(function() {
-               $("#secure").css('background-image',brImage) }, 800*i);
+               $("#secure").css('background-image',brImage) }, 300*i);
 
         })(i);
         //$("#secure").css({'background-image':'url(/images/bgimages/swa-tail-animation-3-test-FINISH.jpg)'});
@@ -37,6 +38,58 @@ $(document).ready(function(){
 
 
 
+	*/
+	
+	
+var imgAll = [
+             
+			'images/bgimages/swa_intro-animation-1-test.jpg', 
+            'images/bgimages/swa_intro-animation-2-test.jpg',
+
+            'images/bgimages/swa_intro-animation-3-test.jpg',
+
+            'images/bgimages/swa_intro-animation-4-test.jpg',
+
+            'images/bgimages/swa_intro-animation-5-test.jpg',
+			'images/bgimages/swa_intro-animation-6-test.jpg',
+            'images/bgimages/swa-scroll-down-btn-animation-1-test.jpg',
+            'images/bgimages/swa-scroll-down-btn-animation-2-test.jpg',
+            'images/bgimages/swa-scroll-down-btn-animation-3-test.jpg',
+            'images/bgimages/swa-scroll-down-btn-animation-4-test.jpg',
+            'images/bgimages/swa-tail-animation-1-test.jpg',
+            'images/bgimages/swa-tail-animation-2-test.jpg',
+            'images/bgimages/swa-tail-animation-3-test.jpg',
+             'images/bgimages/background_finish.png'
+			], i=1;
+
+    function createAnimation(){
+
+ 
+        if(i > (imgAll.length-1)){
+
+            clearInterval(intervalGo); 
+
+        }else{
+			
+			
+            $('#secure').animate({'opacity':'0'},20,function(){
+
+                $('#secure').css({'background':'url('+imgHead[i]+')'});
+
+                i++;
+
+            });
+            
+            $('#secure').animate({'opacity':'1'},50);
+        }
+
+         
+
+    }
+
+    var intervalGo = setInterval(createAnimation,1000);
+	
+	
 
 
 
@@ -45,9 +98,3 @@ $(document).ready(function(){
 });
 
 
-
-/*function func() {
-    alert( 'Привет' );
-}
-
-setTimeout(func, 1000); */
