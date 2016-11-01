96516019bcfe6db6205e71dae7d28fc6347a2c40
Vladimir Nimchyn <nima.vova@gmail.com>
2016-11-02 00:29:12 +0200

    add all files

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+/**
+ * Created by nima on 29.10.16.
+ */
+
+
+$(document).ready(function(){
+
+
+
+
+    for(var i = 2; i <= 15; i++) {
+
+
+        (function(i) {
+            if (i==2) var brImage='url(/images/bgimages/swa_intro-animation-2-test.jpg)';
+            if (i==3) var brImage='url(/images/bgimages/swa_intro-animation-3-test.jpg)';
+            if (i==4) var brImage='url(/images/bgimages/swa_intro-animation-4-test.jpg)';
+            if (i==5) var brImage='url(/images/bgimages/swa_intro-animation-5-test.jpg)';
+            if (i==6) var brImage='url(/images/bgimages/swa_intro-animation-6-test.jpg)';
+            if (i==7) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-1-test.jpg)';
+            if (i==8) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-1-test1.jpg)';
+            if (i==9) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-2-test.jpg)';
+            if (i==10) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-3-test.jpg)';
+            if (i==11) var brImage='url(/images/bgimages/swa-scroll-down-btn-animation-4-test.jpg)';
+            if (i==12) var brImage='url(/images/bgimages/swa-tail-down-btn-animation-1-test.jpg)';
+            if (i==13) var brImage='url(/images/bgimages/swa-tail-down-btn-animation-2-test.jpg)';
+            if (i==14) var brImage='url(/images/bgimages/swa-tail-animation-3-test.jpg)';
+            if (i==15) var brImage='url(/images/bgimages/swa-tail-animation-3-test-FINISH.jpg)';
+
+
+       setTimeout(function() {
+               $("#secure").css('background-image',brImage) }, 800*i);
+
+        })(i);
+        //$("#secure").css({'background-image':'url(/images/bgimages/swa-tail-animation-3-test-FINISH.jpg)'});
+    }
+
+
+
+
+
+
+
+
+});
+
+
+
+/*function func() {
+    alert( 'Привет' );
+}
+
+setTimeout(func, 1000); */
